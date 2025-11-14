5360c206f540c4d2278faacdac91b76121687cc8
Luis Antonio Marroquin Salazar <lmarroquin4567@gmail.com>
2025-11-14 14:49:36 -0800

    Configure Vite with bundle visualizer plugin
    - Added visualizer plugin to generate stats.html for bundle analysis
    - Enables identification of large modules and optimization opportunities

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest.json' assert { type: 'json' };
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   plugins: [
     preact(),
     crx({ manifest })
+        visualizer({ filename: './stats.html', open: true })
   ],
   build: {
     rollupOptions: {
